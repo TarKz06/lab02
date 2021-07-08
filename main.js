83984ec7728b5381137dd310aabbe13f3fd11695
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-08 18:32:28 +0700

    3.3

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            brand: 'SE 331',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
+            brand: 'TarKz ',
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
             ],
-            cart: 0
-
+            cart: 0,
+            selectedVariant:0
         }
     },
     methods: {
@@ -21,11 +19,21 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant (index){
+            this.selectedVariant = index;
         }
     },
     computed:{
         title(){
             return this.brand +' ' +this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
         }
-    }
+    },
+    
 })
