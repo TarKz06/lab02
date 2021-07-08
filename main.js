98a9e8a18d135a19008bc5ea37c4386be4891b2d
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-08 19:26:10 +0700

    4.6

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 const app = Vue.createApp({
     data() {
-        return {       
-            cart: 0,    
+        return {
+            // product: 'Shoes',
+            // brand: 'TarKz ',
+            // inventory: 100,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
+            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
+            // ],
+            // selectedVariant:0,
+            // onSale: true,
+            cart: 0
+            
+    //     }
+    // },
+    // methods: {
+    //     addToCart() {
+    //         this.cart += 1
+    //     },
+    //     updateImage(variantImage) {
+    //         this.image = variantImage
+    //     },
+    //     updateVariant (index){
+    //         this.selectedVariant = index;
+    //     }
+    // },
+    // computed:{
+    //     title(){
+    //         if(this.variants[this.selectedVariant].quantity > 0){
+    //             return this.brand + ' ' + this.product + " In Stock"
+    //         }else {
+    //             return this.brand + ' ' + this.product + " Out Stock"
+    //         }
+    //     },
+    //     image(){
+    //         return this.variants[this.selectedVariant].image
+    //     },
+    //     inStock(){
+    //         return this.variants[this.selectedVariant].quantity
         }
     },
     //
