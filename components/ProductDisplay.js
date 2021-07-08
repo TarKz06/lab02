dee04cea8eb329cc1c42500b9da64dfaeda8b045
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-08 19:23:07 +0700

    4.3-4.5

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,30 +1,63 @@
 app.component('product-display',{
     template:
     /*html*/
-    `<div class="product-display">
-        <div class="product-container">
-            <div class="product-image">
-                <img v-bind:src="image">
-            </div>
-            <div class="product-info">
-                <h1>{{title}}</h1>
-                <p v-if="inStock">In Stock</p>
-                <p v-else>Out of Stock</p>
-                <div 
-                    v-for="(variant,index) in variants" 
-                    :key="variant.id" 
-                    @mouseover="updateVariant(index)" 
-                    class="color-circle" 
-                    :style="{backgroundColor: variant.color}">
-                </div>
-                <button 
-                    class=" button " 
-                    :disabled='!inStock' 
-                    :class="{disabledButton: !inStock} " 
-                    v-on:click="addToCart">
-                    Add to Cart
-                </button>
-            </div>
+    `   <div class="product-display">
+    <div class="product-container">
+        <div class="product-image">
+            <img :src="image" :disabled='!inStock':class="{disabledImage: !inStock} " >
         </div>
-    </div>`
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+            <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant(index)" class="color-circle":style="{backgroundColor:variant.color}"></div>
+            <button class=" button " :disabled='!inStock':class="{disabledButton: !inStock} "@click="addToCart ">Add to Cart</button>
+        </div>
+    </div>
+</div>`,
+data() {
+    return {
+        product: 'Shoes',
+        brand: 'TarKz ',
+        inventory: 100,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
+        ],
+        cart: 0,
+        selectedVariant:0,
+        onSale: true
+    }
+},
+methods: {
+    addToCart() {
+        this.cart += 1
+    },
+    updateImage(variantImage) {
+        this.image = variantImage
+    },
+    updateVariant (index){
+        this.selectedVariant = index;
+    }
+},
+computed:{
+    title(){
+        if(this.variants[this.selectedVariant].quantity > 0){
+            return this.brand + ' ' + this.product + " In Stock"
+        }else {
+            return this.brand + ' ' + this.product + " Out Stock"
+        }
+    },
+    image(){
+        return this.variants[this.selectedVariant].image
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].quantity
+    }
+},
 })
